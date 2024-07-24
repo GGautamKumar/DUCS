2ba6272ab27c694e179ff5b26b346240d82f18c1
Gautam <gautam.mca23.du@gmail.com>
2024-07-24 19:30:14 +0530

    first commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./footer.css"
+
+const footer = () => {
+  return (
+    <div className='Footer'>
+        <p>
+            @ 2024 DUCS,University of Delhi. All rights reserved.
+        </p>
+        <ul>
+            <li>
+                Terms of Services
+            </li>
+            <li>
+                Privacy Policy
+            </li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default footer;
